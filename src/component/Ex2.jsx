1d77c2757791e53786fa74291bfe8b6965da8675
SarikaPrashanth25 <sarikasneha1920@gmail.com>
2023-07-10 21:44:28 +0530

    first commit

---
+++ src/component/Ex2.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+/* const local functional component */
+const Ex2 = function (props) {
+   
+    const [x,setX] = useState(1) // mutable states
+
+    const[title,setTitle] = useState('Welcome to state')
+
+    const[view,setView] = useState(false)
+
+    const[users,setUsers] = useState(['Prashu','Pacchi','Sarika'])
+
+    const[emp,setEmp] = useState({
+        name: 'Sarika',
+        email: 'sarika@gmail.com'
+    })
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 text-center">
+                    <h4 className="display-4 text-warning">useState</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="text-success"> x = {x} </h1>
+                    <hr />
+                    <h4 className="text-warning"> { title } </h4>
+                    <hr />
+                    <h4 className={ view ? "text-success": "text-danger"}>
+                        view = { view ? "Say True" : "Say False"}
+                    </h4>
+                    <hr />
+
+                    <ul className="list-group">
+                        {
+                            users.map((item,index) => {
+                                return (
+                                    <li className="list-group-item" key={index}> { item } </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <hr />
+                    <h4 className="text-info"> { emp.name } { emp.email } </h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Ex2
